a6e55b8eb70b00005174c30d2d661064338ed5e2
Angelo Caputo <a.caputo1991@gmail.com>
2016-11-22 00:07:51 +0100

    Completati in tutte le funzionalità (insert,delete,update,get) per i gestori delle Partite e dei Tornei

--- src/AppBundle/Controller/PartitaController.php
+++ src/AppBundle/Controller/PartitaController.php
@@ -10,16 +10,18 @@ namespace AppBundle\Controller;
 
 
 use AppBundle\Manager\ManagerPartita;
+use AppBundle\Model\Partita;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class PartitaController extends Controller
 {
 
     /**
-     * @Route("/partite/all",name="allPartite")
+     * @Route("/partiteAll",name="tutteLePartite")
      * @Method("GET")
      */
     public function getAllPartite(){
@@ -34,6 +36,111 @@ class PartitaController extends Controller
         }
         else {
             return new Response("problemi");
+        }
+    }
+
+    /**
+     * @Route("/partite/{id}",name="allPartite")
+     * @Method("GET")
+     */
+    public function getPartitaById($id){
+        $managerPartite = new ManagerPartita();
+        $risultato = $managerPartite->getPartitaById($id);
+        if($risultato!=null){
+            return new Response($risultato);
+        } else {
+            return new Response("La partita con questo id: ".$id." non esiste");
+        }
+    }
+
+    /**
+     * @Route("/partite/getSquadraCasa/{id}",name="allPartiteSquadraCasa")
+     * @Method("GET")
+     */
+    public function getPartiteBySquadraCasa($id){
+        $managerPartite = new ManagerPartita();
+        $risultato = $managerPartite->getPartitaBySquadraCasa($id);
+        if($risultato!=null){
+            $toRetutn = "";
+            foreach ($risultato as $s){
+                $toRetutn = $toRetutn.$s;
+            }
+            return new Response($toRetutn);
+        } else {
+            return new Response("La partita con questo id: ".$id." non esiste");
+        }
+    }
+
+    /**
+     * @Route("/partite/getSquadraTrasferta/{id}",name="allPartiteSquadraCasa")
+     * @Method("GET")
+     */
+    public function getPartiteBySquadraTrasferta($id){
+        $managerPartite = new ManagerPartita();
+        $risultato = $managerPartite->getPartitaBySquadraTrasferta($id);
+        if($risultato!=null){
+            $toRetutn = "";
+            foreach ($risultato as $s){
+                $toRetutn = $toRetutn.$s;
+            }
+            return new Response($toRetutn);
+        } else {
+            return new Response("La partita con questo id: ".$id." non esiste");
+        }
+    }
+
+    /**
+     * @Route("/partite/inserisciPartita",name="inserisciPartita")
+     * @Method("POST")
+     */
+    public function insertPartita(Request $req){
+        $partita = new Partita();
+        $managerPartita = new ManagerPartita();
+        $squadraCasa = $req->request->get("squadraCasa");
+        $squadraTrasferta = $req->request->get("squadraTrasferta");
+        $partita->setSquadraCasa($squadraCasa);
+        $partita->setSquadraTrasferta($squadraTrasferta);
+        $partita->setRisultato($req->request->get("risultato"));
+        $partita->setSquadreIdSquadre($req->request->get("squadraEsterna"));
+        $ris = $managerPartita->insertPartita($partita);
+        if ($ris != null) {
+            return new Response("Inserimento avvenuto con successo");
+        } else {
+            return new Response("Problemi con l'inserimento");
+        }
+    }
+
+    /**
+     * @Route("/partite/cancellaPartita/{id}",name="inserisciPartita")
+     * @Method("GET")
+     */
+    public function deletePartita($id){
+        $managerPartite = new ManagerPartita();
+        $ris = $managerPartite->deletePartita($id);
+        if($ris!=null){
+            return new Response("Cancellazione di partita con questo id: ".$id." avvenuta con successo");
+        } else {
+            return new Response("Problemi con la cancellazione");
+        }
+    }
+
+    /**
+     * @Route("/partite/aggiornaPartita",name="aggiornaPartita")
+     * @Method("POST")
+     */
+    public function aggiornaPartita(Request $req){
+        $managerPartite = new ManagerPartita();
+        $partita = new Partita();
+        $partita->setIdPartita($req->request->get("idPartita"));
+        $partita->setSquadraCasa($req->request->get("squadraCasa"));
+        $partita->setSquadraTrasferta($req->request->get("squadraTrasferta"));
+        $partita->setRisultato($req->request->get("risultato"));
+        $partita->setSquadreIdSquadre($req->request->get("squadraEsterna"));
+        $ris = $managerPartite->aggiornaPartita($partita);
+        if($ris!=null){
+            return new Response("Aggiornamento avvenuto con successo");
+        } else {
+            return new Response("Problemi con l'aggiornamento");
         }
     }
 }
--- src/AppBundle/Manager/ManagerPartita.php
+++ src/AppBundle/Manager/ManagerPartita.php
@@ -47,6 +47,114 @@ class ManagerPartita
         }
     }
 
+    public function getPartitaById($idPartita){
+        $toReturnPartita = new Partita();
+        $sql = "SELECT * FROM partite WHERE idPartita='$idPartita'";
+        $result = $this->conn->query($sql);
+        $valore = $result->num_rows;
+        if($valore > 0){
+            $row = $result->fetch_assoc();
+            $toReturnPartita->setSquadreIdSquadre($row["Squadre_idSquadre"]);
+            $toReturnPartita->setRisultato($row["risultato"]);
+            $toReturnPartita->setSquadraCasa($row["squadraCasa"]);
+            $toReturnPartita->setSquadraTrasferta($row["squadraTrasferta"]);
+            $toReturnPartita->setIdPartita($row["idPartita"]);
+            return $toReturnPartita;
+        } else {
+            return null;
+        }
+
+    }
+
+    public function getPartitaBySquadraCasa($squadraCasa){
+        $toReturnPartite = array();
+        $sql = "SELECT * FROM partite WHERE squadraCasa='$squadraCasa'";
+        $result = $this->conn->query($sql);
+        if($result->num_rows > 0){
+            $i=0;
+            while($row = $result->fetch_assoc()){
+                $s = new Partita();
+                $s->setIdPartita($row["idPartita"]);
+                $s->setSquadraCasa($row["squadraCasa"]);
+                $s->setSquadraTrasferta($row["squadraTrasferta"]);
+                $s->setRisultato($row["risultato"]);
+                $s->setSquadreIdSquadre($row["Squadre_idSquadre"]);
+                $toReturnPartite[$i] = $s;
+                $i++;
+            }
+            return $toReturnPartite;
+        } else {
+            return null;
+        }
+    }
+
+    public function getPartitaBySquadraTrasferta($squadraTrasferta){
+        $toReturnPartite = array();
+        $sql = "SELECT * FROM partite WHERE squadraTrasferta='$squadraTrasferta'";
+        $result = $this->conn->query($sql);
+        if($result->num_rows > 0){
+            $i=0;
+            while($row = $result->fetch_assoc()){
+                $s = new Partita();
+                $s->setIdPartita($row["idPartita"]);
+                $s->setSquadraCasa($row["squadraCasa"]);
+                $s->setSquadraTrasferta($row["squadraTrasferta"]);
+                $s->setRisultato($row["risultato"]);
+                $s->setSquadreIdSquadre($row["Squadre_idSquadre"]);
+                $toReturnPartite[$i] = $s;
+                $i++;
+            }
+            return $toReturnPartite;
+        } else {
+            return null;
+        }
+    }
+
+    public function insertPartita(Partita $partita){
+        $managerSquadra = new ManagerSquadra();
+        $squadraCasa = $partita->getSquadraCasa();
+        $squadraTrasferta = $partita->getSquadraTrasferta();
+        if($managerSquadra->getByName($squadraCasa) AND $managerSquadra->getByName($squadraTrasferta)) {
+            $sql = "INSERT INTO partite (squadraCasa, squadraTrasferta, risultato, Squadre_idSquadre) VALUES ('" . $partita->getSquadraCasa() . "','" . $partita->getSquadraTrasferta() . "','" . $partita->getRisultato() . "','" . $partita->getSquadreIdSquadre() . "')";
+            $risultato = $this->conn->query($sql);
+            return $risultato;
+        } else {
+            return null;
+        }
+    }
+
+    public function deletePartita($idPartita){
+        $sql = "DELETE FROM partite WHERE idPartita='$idPartita'";
+        $risultato = $this->conn->query($sql);
+        if($risultato!=null){
+            return $risultato;
+        } else {
+            return null;
+        }
+    }
+
+    public function aggiornaPartita(Partita $partita){
+        $managerPartita = new ManagerPartita();
+        $risultato = $managerPartita->getPartitaById($partita->getIdPartita());
+        if($risultato!=null){
+            $risultato = $partita->getRisultato();
+            $squadraCasa = $partita->getSquadraCasa();
+            $squadraTrasferta = $partita->getSquadraTrasferta();
+            $sqEx = $partita->getSquadreIdSquadre();
+            $id = $partita->getIdPartita();
+            $sql = "UPDATE partite SET risultato='$risultato', squadraCasa='$squadraCasa' , squadraTrasferta='$squadraTrasferta' , Squadre_idSquadre='$sqEx' WHERE idPartita='$id'";
+            $ri = $this->conn->query($sql);
+            if($ri!=null){
+                return $ri;
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+
     public function __destruct()
     {
         // TODO: Implement __destruct() method.
